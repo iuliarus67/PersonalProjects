ae4e5adf3d592d4cb1a37b9605947707f237c616
iuliarus67 <79667911+iuliarus67@users.noreply.github.com>
2022-01-20 20:44:59 +0200

    Delete CustomerLoginRepository.java

--- CustomerLoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.proiectse.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerLoginRepository extends CrudRepository<CustomerLogin, Integer> {
-    public CustomerLogin findByEmail(String email);
-}
