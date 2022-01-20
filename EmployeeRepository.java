abc879ca519a9fa2f17b3444ffa0094573d38c04
iuliarus67 <79667911+iuliarus67@users.noreply.github.com>
2022-01-20 20:43:36 +0200

    Delete EmployeeRepository.java

--- EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.proiectse.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
-
-    public Employee findByEmail(String email);
-}
