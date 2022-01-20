227d1a99ea91d3ed26f1f090b2ba6756c61ad573
iuliarus67 <79667911+iuliarus67@users.noreply.github.com>
2022-01-20 21:19:10 +0200

    Delete BookNotFoundException.java

--- BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.proiectse.exception;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String title) {
-        super("The book with title : " + title + " cannot be found");
-    }
-}
